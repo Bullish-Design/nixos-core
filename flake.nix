25029d9d88638fcb78f9d045c51124323b9580c2
Bullish-Design <97364799+Bullish-Design@users.noreply.github.com>
2025-12-29 17:15:45 -0500

    Add initial flake.nix configuration for NixOS

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "NixOS core modules (WSL only)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixos-wsl, ... }: {
+    nixosModules = {
+      wsl-upstream = nixos-wsl.nixosModules.default;
+      common = import ./modules/common.nix;
+      wsl = import ./modules/wsl.nix;
+    };
+  };
+}
